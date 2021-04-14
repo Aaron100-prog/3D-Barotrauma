cf20e3a3e69f4c2ea8aaea927969424a0ff2856f
Aaron <aaron.golder@gmx.de>
2021-04-14 16:47:59 +0200

    Add: SceneMangerScript hinzugefügt
    Ein einfacher Script zum starten von Scenen sowie dem Schließen des Spiels.

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void StartScene(int Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Spiel geschlossen");
+    }
+}

5e9d5b361667e47df1a403e323a496541f2c2ec3
Aaron <aaron.golder@gmx.de>
2021-10-03 18:47:37 +0200

    Add: Kamera Steuerung zu Freecam hinzugefügt
    Kamera Steuerung zu Freecam hinzugefügt.

--- Assets/Scripts/FreecamController.cs
+++ Assets/Scripts/FreecamController.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class FreecamController : MonoBehaviour
 {
+    [Header("Camera Movement")]
+    private float sensitivity = 180f;
+    [HideInInspector]
+    private float yRotation = 0f;
+    private float xRotation = 0f;
+    [HideInInspector]
+    public Camera Camera;
+
     PlayerController Controller;
     public void GetPlayer(GameObject ParsedPlayer)
     {
         Controller = ParsedPlayer.gameObject.GetComponent<PlayerController>();
     }
+    private void Awake()
+    {
+        Camera = this.gameObject.GetComponent<Camera>();
+    }
 
     private void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        yRotation -= mouseY;
+        xRotation += mouseX;
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+        this.transform.localRotation = Quaternion.Euler(yRotation, xRotation, 0f);
+
         if (Input.GetKey(KeyCode.F3))
         {
             if (Input.GetKeyDown(KeyCode.F))

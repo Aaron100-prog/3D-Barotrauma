ea727fdc8a2d7a1bc921338d4b1f1fe301bd7924
Aaron <aaron.golder@gmx.de>
2021-10-16 23:41:00 +0200

    Add: Begonnen Hüllen Script zu erstellen
    Begonnen Hüllen Script zu erstellen.

---
+++ Assets/Scripts/Hull.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hull : MonoBehaviour
+{
+    public float OxygenInHull = 100f;
+    public float WaterLevelInHull = 0f;
+    public float PressureInHull = 0f;
+}

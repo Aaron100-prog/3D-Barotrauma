b511baf3b5dcc749b8d6b1e654a6fa27fffea154
Aaron <aaron.golder@gmx.de>
2021-10-19 00:14:06 +0200

    Add: Tooltipps zu neuem Script hinzugefügt
    Tooltipps zu neuem Script hinzugefügt.

--- Assets/Scripts/RestrictionButton.cs
+++ Assets/Scripts/RestrictionButton.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class RestrictionButton : Interactable
 {
+    [Tooltip("Text der angezeigt wird wenn der Spieler mit dem Cursor über dem Knopf ist")]
     public string ButtonDescription;
 
+    [Tooltip("Müssen alle nicht zugelassen Text Strings übereinstimmen damit der Knopfdruck nicht erfolgreich ist")]
     public bool CheckallInvalidAccess;
+    [Tooltip("Nicht zugelasse Text Strings für erfolgreiches drücken des Knopfes")]
     public string[] ButtonInvalidAccess;
 
+    [Tooltip("Müssen alle zugelassen Text Strings übereinstimmen damit der Knopfdruck erfolgreich ist")]
     public bool CheckallValidAccess;
+    [Tooltip("Zugelasse Text Strings für erfolgreiches drücken des Knopfes")]
     public string[] ButtonValidAccess;
 
+    [Tooltip("Objekt welches nach drücken des Knopfes mit ausreichendem Zugriff aktiviert/umgeschalten wird")]
     public GameObject ActivatedObject;
 
     [HideInInspector]

230bdc28d79d8b0602cd2728974db3e00f914f36
Aaron <aaron.golder@gmx.de>
2021-11-20 14:17:16 +0100

    Add: Hüllen berechnen jetzt anhand ihrer Größe die Maximale Menge an Sauerstoff
    Hüllen berechnen jetzt anhand ihrer Größe die Maximale Menge an Sauerstoff.

--- Assets/Scripts/Hull.cs
+++ Assets/Scripts/Hull.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Hull : MonoBehaviour
 {
-    private float MaxOxygenInHull = 1000f;
-    public float OxygenInHull = 1000f;
+    public float MaxOxygenInHull;
+    public float OxygenInHull;
     public float WaterLevelInHull = 0f;
     public float PressureInHull = 0f;
+    float Volume;
+    BoxCollider coll;
 
     public void Start()
     {
         Fillroomwithoxy();
     }
+    private void CalculateMaxOxygen()
+    {
+        coll = GetComponent<BoxCollider>();
+        MaxOxygenInHull = coll.size.x * coll.size.y * coll.size.z * 25f;
+    }
 
     public void Fillroomwithoxy()
     {
+        CalculateMaxOxygen();
         OxygenInHull = MaxOxygenInHull;
     }
 }

ac9f6faac80c208bbefdea128839f632bb57b166
Aaron <aaron.golder@gmx.de>
2021-11-30 18:27:46 +0100

    Add: Hüllen erschaffen jetzt ein zu ihrer Größe passendes Wasser Objekt welches gescaled werden kann
    Hüllen erschaffen jetzt ein zu ihrer Größe passendes Wasser Objekt welches gescaled werden kann.

--- Assets/Scripts/Hull.cs
+++ Assets/Scripts/Hull.cs
@@ -10,9 +10,14 @@ public class Hull : MonoBehaviour
     public float PressureInHull = 0f;
     float Volume;
 
+    private BoxCollider Collider;
+    private GameObject WaterParent;
+    private GameObject Water;
+
     public void Start()
     {
         Fillroomwithoxy();
+        CreateWater();
     }
     private void CalculateMaxOxygen()
     {
@@ -24,5 +29,23 @@ public class Hull : MonoBehaviour
     {
         CalculateMaxOxygen();
         OxygenInHull = MaxOxygenInHull;
+    }
+
+    private void CreateWater()
+    {
+        Collider = GetComponent<BoxCollider>();
+        WaterParent = new GameObject("WaterParent " + name);
+        WaterParent.transform.SetParent(transform);
+        WaterParent.transform.localPosition = new Vector3(0, -(Collider.size.y/2) + Collider.center.y, 0);
+
+        Water = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Water.name = "Water";
+        Water.transform.SetParent(WaterParent.transform);
+        Water.transform.localPosition = new Vector3(Collider.center.x, Collider.center.y + -WaterParent.transform.localPosition.y, Collider.center.z);
+        Water.transform.localScale = Collider.size;
+        BoxCollider WaterCollider = Water.GetComponent<BoxCollider>();
+        WaterCollider.isTrigger = true;
+
+        WaterParent.transform.localScale = new Vector3(1, 0, 1);
     }
 }

6de81d0375670fc7b8fbc51480c87612b09008c8
Aaron <aaron.golder@gmx.de>
2021-12-04 03:07:36 +0100

    Add: Begonnen Gaps hinzuzufügen
    Begonnen Gaps hinzuzufügen.

--- Assets/Scripts/Gap.cs
+++ Assets/Scripts/Gap.cs
@@ -6,11 +6,24 @@ public class Gap : MonoBehaviour
 {
     public GameObject Door;
     [SerializeField]
-    Collider[] foundhulls;
-
+    public List<Hull> Hulls;
+    private LayerMask HullMask;
     public void Start()
     {
-        foundhulls = Physics.OverlapBox(this.transform.position, this.transform.localScale, this.transform.rotation, 9, QueryTriggerInteraction.Ignore);
-        
+        HullMask = LayerMask.GetMask("Hull");
+        Collider[] foundhullcollider = Physics.OverlapBox(new Vector3(0, 0, 0), new Vector3(1,1,1), transform.rotation, HullMask, QueryTriggerInteraction.Collide);
+        for (int i = 0; i < foundhullcollider.Length; i++)
+        {
+            
+            if (foundhullcollider[i].GetComponent<Hull>() == null)
+            {
+                Debug.Log(gameObject.name + ": " + foundhullcollider[i].gameObject.name + " besitzt keinen Hüllen Script!");
+            }
+            else
+            {
+                Hulls.Add(foundhullcollider[i].GetComponent<Hull>());
+            }
+
+        }
     }
 }

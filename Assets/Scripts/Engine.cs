edf201a09d659f00cb9ca3452812a3ad076b53d2
Aaron <aaron.golder@gmx.de>
2022-04-11 15:25:03 +0200

    Add: Beschleunigung der Antriebe hinzugefügt
    Beschleunigung der Antriebe hinzugefügt.

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -8,13 +8,52 @@ public class Engine : MonoBehaviour
     public float ID = 1;
     private float neededpower = 15;
     private bool active = true;
-    public float throttle = 0;
+    public float targetthrottle = 0;
+    private float activthrottle;
+    public float throttlechange = 2f;
 
     public void Update()
     {
-        if(active)
+        if (active)
         {
-            this.transform.parent.parent.transform.position = this.transform.parent.parent.transform.position + (transform.right * throttle/10 * Time.deltaTime);
+            if (activthrottle != targetthrottle)
+            {
+                if (Mathf.RoundToInt(activthrottle) > targetthrottle)
+                {
+                    float difference = (targetthrottle - activthrottle) * -1;
+                    Debug.Log(difference);
+                    if (difference > throttlechange)
+                    {
+                        activthrottle -= throttlechange * Time.deltaTime;
+                    }
+                    else
+                    {
+                        activthrottle -= difference * Time.deltaTime;
+                        if (System.Math.Round(difference, 2) == 0.00)
+                        {
+                            activthrottle = targetthrottle;
+                        }
+                    }
+                }
+                else
+                {
+                    float difference = targetthrottle - activthrottle;
+                    Debug.Log(difference);
+                    if (difference > throttlechange)
+                    {
+                        activthrottle += throttlechange * Time.deltaTime;
+                    }
+                    else
+                    {
+                        activthrottle += difference * Time.deltaTime;
+                        if (System.Math.Round(difference, 2) == 0.00)
+                        {
+                            activthrottle = targetthrottle;
+                        }
+                    }
+                }
+            }
+            this.transform.parent.parent.transform.position = this.transform.parent.parent.transform.position + (transform.right * activthrottle / 10 * Time.deltaTime);
         }
     }
 }
